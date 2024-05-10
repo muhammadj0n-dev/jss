d854508e9dccf6caa3cf355d0ddfbc1a88ce494b
muhammadj0n-dev <153054724+muhammadj0n-dev@users.noreply.github.com>
2024-05-11 02:08:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// 48
+// let arr = "George Raymond Richard Martin".split(" ");
+// let str = arr.map(el =>el[0])
+// console.log(str.join());
+
+// 65
+// let obj = {a: 2, b: 5, c: 7};
+// let arr = Object.entries(obj);
+// arr.forEach(el => {
+//     let new_arr = [...el];
+//     let str = new_arr.join();
+//     console.log(str);
+// })
+
+// 68
+// let arr =  [false, 1, 10, "", null, "sultonqul", 1.13, 0];
+// let truthy = [];
+// let falsy = [];
+// for(let i =0; i < arr.length; i++){
+//     if(arr[i]){
+//         truthy.push(arr[i])
+//     }else{
+//         falsy.push(arr[i])
+//     }
+// }
+// console.log(truthy);
+// console.log(falsy);
+
+
+// 44
+// function getLevel(n){
+// for(let i =0; i < n; i++){
+//     let num = Math.pow(2, i+2);
+//     console.log(num);
+// }
+// }
+// let n = 4;
+// getLevel(n);
+
+
+// function getLevel(n){
+//     for(let i =0; i < n; i++){
+//         let num = Math.pow(2, i+2);
+//         console.log(num);
+//     }
+//     }
+//     let n = 5;
+//     getLevel(n);
+
+// 43
+// let obj = {a: 2, b: 3, c: 4};
+// for(let x in obj){
+//     obj[x]*=3; 
+// }
+// console.log(obj);
+
+// 42
+// let arr =  ["text", "world", "laptop"];
+// let obj ={};
+// for(let i = 0; i<arr.length; i++){
+//     let str = (arr[i]);
+//     obj[str] =str.length;
+// }
+// console.log(obj);
+
+// 40
+// let arr =[1,2,3,4,5] 
+// let num = arr.reduce((total , current )=> total + current);
+// console.log(num/arr.length);
+
+// 38
+// let strFn =(text)=>{
+//     let sum =0
+// for(let i =0; i<text.length;i++){
+//     let lett = text[i].split(" ");
+//     for (let j = 0; j < lett.length; j++) {
+//         if (lett[j].includes("a")) {
+//             sum++;
+//         }
+// }
+// }
+// console.log(sum);
+// }
+// let arr = ["mann juda yaxshi bolaman"]
+// strFn(arr)  
+
+
+
+
